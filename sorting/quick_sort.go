416bdcd7def27a3ad1da2ea1ce5d02c97f2ea8e5
bbenzo <contact@benbarten.com>
2020-09-28 08:40:00 +0200

    add comments

--- sorting/quick_sort.go
+++ sorting/quick_sort.go
@@ -11,19 +11,23 @@ func QuickSort(arr []int) []int {
 	pivot := rand.Int() % len(arr)
 
 	// move pivot to end of array
-	lastIndex := len(arr) - 1
-	arr[pivot], arr[lastIndex] = arr[lastIndex], arr[pivot]
+	high := len(arr) - 1
+	arr[pivot], arr[high] = arr[high], arr[pivot]
 
+	// define beginning index of array higher than pivot and beginning index of array which has not been compared to pivot yet
 	higherThanIndex, unknownIndex := 0, 0
 
-	for unknownIndex < lastIndex {
-		if arr[unknownIndex] <= arr[lastIndex] {
+	for unknownIndex < high {
+		// if element is smaller than pivot, swap element with first element of array higher(!) than pivot and move index of that one up
+		if arr[unknownIndex] <= arr[high] {
 			arr[higherThanIndex], arr[unknownIndex] = arr[unknownIndex], arr[higherThanIndex]
 			higherThanIndex++
 		}
+
 		unknownIndex++
 	}
 
+	// recursively sort the smaller and higher than pivot sub arrays
 	left, right := QuickSort(arr[:higherThanIndex]), QuickSort(arr[higherThanIndex:])
 	return append(left, right...)
 }
