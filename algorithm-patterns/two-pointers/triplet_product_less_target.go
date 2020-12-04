d1635ef5761436b4b0c7084d4bf8b4f6dbbc1a49
bbenzo <contact@benbarten.com>
2020-12-04 14:29:07 +0100

    add triplets with product less than target

--- algorithm-patterns/two-pointers/triplet_product_less_target.go
+++ algorithm-patterns/two-pointers/triplet_product_less_target.go
@@ -1,1 +1,24 @@
 package two_pointers
+
+func TriplestWithProductLessTarget(arr []int, target int) [][]int {
+	result := [][]int{}
+
+	for i := 0; i < len(arr); i++ {
+		product := 1
+
+		j := i
+		tempArr := []int{}
+		for product < target && j < len(arr) {
+			product *= arr[j]
+
+			if product < target {
+				tempArr = append(tempArr, arr[j])
+				result = append(result, tempArr)
+			}
+
+			j++
+		}
+	}
+
+	return result
+}
--- algorithm-patterns/two-pointers/triplet_product_less_target_test.go
+++ algorithm-patterns/two-pointers/triplet_product_less_target_test.go
@@ -1,1 +1,24 @@
 package two_pointers
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestTripletsWithProductLessTarget(t *testing.T) {
+	input := []int{2, 5, 3, 10}
+	target := 30
+
+	expected := [][]int{{2}, {2, 5}, {5}, {5, 3}, {3}, {10}}
+
+	assert.Equal(t, expected, TriplestWithProductLessTarget(input, target))
+}
+
+func TestTripletsWithProductLessTarget2(t *testing.T) {
+	input := []int{8, 2, 6, 5}
+	target := 50
+
+	expected := [][]int{{8}, {8, 2}, {2}, {2, 6}, {6}, {6, 5}, {5}}
+
+	assert.Equal(t, expected, TriplestWithProductLessTarget(input, target))
+}
