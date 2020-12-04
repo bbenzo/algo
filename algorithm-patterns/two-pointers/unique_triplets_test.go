9bcea723e5cdf9079902af06f22da98144aca317
bbenzo <contact@benbarten.com>
2020-12-04 14:28:51 +0100

    fix order of test result

--- algorithm-patterns/two-pointers/unique_triplets_test.go
+++ algorithm-patterns/two-pointers/unique_triplets_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestUniqueTriplets(t *testing.T) {
 	input := []int{-3, 0, 1, 2, -1, 1, -2} // sorted = -3, -2, -1, 0, 1, 1, 2
-	expected := [][]int{{-3, 1, 2}, {-2, 0, 2}, {-2, 1, 1}, {-1, 0, 1}}
+	expected := [][]int{{-3, 2, 1}, {-2, 2, 0}, {-2, 1, 1}, {-1, 1, 0}}
 
 	result := UniqueTripletsThatAddUpToZero(input)
 
