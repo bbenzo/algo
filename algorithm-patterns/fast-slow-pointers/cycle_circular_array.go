928547dc7c2a0bea51f721d299c144a642cbae29
bbenzo <contact@benbarten.com>
2020-12-20 13:46:29 +0100

    finalize cycle circular array

--- algorithm-patterns/fast-slow-pointers/cycle_circular_array.go
+++ algorithm-patterns/fast-slow-pointers/cycle_circular_array.go
@@ -7,25 +7,51 @@ func CycleCircularArray(arr []int) bool {
 
 	slow, fast := 0, 0
 	for slow < len(arr) {
+		oneDirection := true
+
 		slow += arr[slow]
 		if slow >= len(arr) {
 			slow -= len(arr)
 		}
 
-		fast += arr[fast]
-		if fast >= len(arr) {
-			fast -= len(arr)
-		}
+		slowToTheRight := goesToTheRight(arr, slow)
 
 		fast += arr[fast]
 		if fast >= len(arr) {
 			fast -= len(arr)
 		}
 
-		if slow == fast {
+		fastOneToTheRight := goesToTheRight(arr, fast)
+
+		fast += arr[fast]
+		if fast >= len(arr) {
+			fast -= len(arr)
+		}
+
+		fastTwoToTheRight := goesToTheRight(arr, fast)
+
+		if !slowToTheRight || !fastOneToTheRight || !fastTwoToTheRight {
+			oneDirection = false
+		}
+
+		if !slowToTheRight && !fastOneToTheRight && !fastTwoToTheRight {
+			oneDirection = true
+		}
+
+		if slow == fast && oneDirection {
 			return true
+		} else if slow == fast && !oneDirection {
+			return false
 		}
 	}
 
 	return false
+}
+
+func goesToTheRight(arr []int, slow int) bool {
+	slowToTheRight := true
+	if arr[slow] < 0 {
+		slowToTheRight = false
+	}
+	return slowToTheRight
 }
