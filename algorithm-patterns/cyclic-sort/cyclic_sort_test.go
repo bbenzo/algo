2612eafa295368cb88e9e26a92c0eb949c6a4694
bbenzo <contact@benbarten.com>
2020-12-27 12:42:05 +0100

    add more tests

--- algorithm-patterns/cyclic-sort/cyclic_sort_test.go
+++ algorithm-patterns/cyclic-sort/cyclic_sort_test.go
@@ -13,3 +13,21 @@ func TestCyclicSort1(t *testing.T) {
 
 	assert.Equal(t, expected, arr)
 }
+
+func TestCyclicSort2(t *testing.T) {
+	arr := []int{2, 6, 4, 3, 1, 5}
+	expected := []int{1, 2, 3, 4, 5, 6}
+
+	CyclicSort(arr)
+
+	assert.Equal(t, expected, arr)
+}
+
+func TestCyclicSort3(t *testing.T) {
+	arr := []int{1, 5, 6, 4, 3, 2}
+	expected := []int{1, 2, 3, 4, 5, 6}
+
+	CyclicSort(arr)
+
+	assert.Equal(t, expected, arr)
+}
