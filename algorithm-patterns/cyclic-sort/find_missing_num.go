2a8e3c93d5610200e9ad8347dbd9b412ceccb89b
bbenzo <contact@benbarten.com>
2020-12-27 12:45:08 +0100

    add find missing num

--- algorithm-patterns/cyclic-sort/find_missing_num.go
+++ algorithm-patterns/cyclic-sort/find_missing_num.go
@@ -1,1 +1,20 @@
 package cyclic_sort
+
+func FindMissingNum(arr []int) int {
+	i := 0
+	for i < len(arr) {
+		if arr[i] != i && arr[i] < len(arr) {
+			arr[i], arr[arr[i]] = arr[arr[i]], arr[i]
+		} else {
+			i++
+		}
+	}
+
+	for i := range arr {
+		if arr[i] != i {
+			return i
+		}
+	}
+
+	return -1
+}
--- algorithm-patterns/cyclic-sort/find_missing_num_test.go
+++ algorithm-patterns/cyclic-sort/find_missing_num_test.go
@@ -1,1 +1,18 @@
 package cyclic_sort
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestFindMissingNum(t *testing.T) {
+	arr := []int{4, 0, 3, 1}
+
+	assert.Equal(t, 2, FindMissingNum(arr))
+}
+
+func TestFindMissingNum2(t *testing.T) {
+	arr := []int{8, 3, 5, 2, 4, 6, 0, 1}
+
+	assert.Equal(t, 7, FindMissingNum(arr))
+}
