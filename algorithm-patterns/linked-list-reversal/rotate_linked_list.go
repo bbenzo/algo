883c7cf3c3b8208440cafd0260872f35ee7de460
bbenzo <contact@benbarten.com>
2020-12-28 18:48:17 +0100

    optimize rotated linked list example

--- algorithm-patterns/linked-list-reversal/rotate_linked_list.go
+++ algorithm-patterns/linked-list-reversal/rotate_linked_list.go
@@ -1,20 +1,26 @@
 package linked_list_reversal
 
 func RotateLinkedList(head *node, k int) *node {
-	current := head
-	var previous *node
-	var next *node
-
-	for k > 0 {
-		next = current.next
-		previous = current
-		previous.next = nil
-
-		current = next
-		current.Push(previous)
-
-		k--
+	if head == nil || head.next == nil {
+		return head
 	}
 
-	return current
+	listLength := 1
+	lastNode := head
+	for lastNode.next != nil {
+		lastNode = lastNode.next
+		listLength++
+	}
+
+	lastNode.next = head
+	k %= listLength
+
+	for i := 0; i < k; i++ {
+		lastNode = lastNode.next
+	}
+
+	head = lastNode.next
+	lastNode.next = nil
+
+	return head
 }
