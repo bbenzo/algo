491a9496d740b0634c7bcddcf33526f795f979d5
Ben Barten <bba@fino.digital>
2021-01-04 11:39:23 +0100

    add all paths for sum

--- algorithm-patterns/tree-depth-first-search/all_paths_for_sum.go
+++ algorithm-patterns/tree-depth-first-search/all_paths_for_sum.go
@@ -1,1 +1,28 @@
 package tree_depth_first_search
+
+func AllPathsForSum(root *node, k int) [][]int {
+	result := [][]int{}
+	current := []int{}
+
+	sum := 0
+	traverseWithResult(root, k, sum, current, &result)
+
+	return result
+}
+
+func traverseWithResult(root *node, k, sum int, current []int, result *[][]int) {
+	if root == nil {
+		return
+	}
+
+	current = append(current, root.val)
+
+	sum += root.val
+	if sum == k {
+		*result = append(*result, current)
+		return
+	}
+
+	traverseWithResult(root.left, k, sum, current, result)
+	traverseWithResult(root.right, k, sum, current, result)
+}
--- algorithm-patterns/tree-depth-first-search/all_paths_for_sum_test.go
+++ algorithm-patterns/tree-depth-first-search/all_paths_for_sum_test.go
@@ -1,1 +1,36 @@
 package tree_depth_first_search
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestAllPathsForSum(t *testing.T) {
+	root := &node{
+		val:   1,
+		left:  &node{
+			val:   7,
+			left:  &node{
+				val:   4,
+			},
+			right: &node{
+				val:   5,
+			},
+		},
+		right: &node{
+			val:   9,
+			left:  &node{
+				val:   2,
+			},
+			right: &node{
+				val:   7,
+			},
+		},
+	}
+
+	sum := 12
+
+	expected := [][]int{{1, 7, 4}, {1, 9, 2}}
+
+ 	assert.Equal(t, expected, AllPathsForSum(root, sum))
+}
