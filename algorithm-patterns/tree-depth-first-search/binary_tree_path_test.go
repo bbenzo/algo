35e4709c141bfb1be1fcc303817a8833d0fd2397
Ben Barten <bba@fino.digital>
2021-01-04 11:29:01 +0100

    add test to binary tree path

--- algorithm-patterns/tree-depth-first-search/binary_tree_path_test.go
+++ algorithm-patterns/tree-depth-first-search/binary_tree_path_test.go
@@ -30,3 +30,29 @@ func TestBinaryTreePath(t *testing.T) {
 
 	assert.True(t, BinaryTreePath(root, 11))
 }
+
+func TestBinaryTreePathFalse(t *testing.T) {
+	root := &node{
+		val:   1,
+		left:  &node{
+			val:   2,
+			left:  &node{
+				val:   4,
+			},
+			right: &node{
+				val:   5,
+			},
+		},
+		right: &node{
+			val:   3,
+			left:  &node{
+				val:   6,
+			},
+			right: &node{
+				val:   7,
+			},
+		},
+	}
+
+	assert.False(t, BinaryTreePath(root, 30))
+}
